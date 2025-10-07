d62ce78a26bf0daf6e656c75b99f8443ed7bf4e9
Alan Duong <alanduong07@gmail.com>
2025-10-07 10:21:08 -0500

    V0.0.21, Windows SendInput migration

--- cyberdriver.py
+++ cyberdriver.py
@@ -166,7 +166,7 @@ async def connect_with_headers(uri, headers_dict):
 
 CONFIG_DIR = ".cyberdriver"
 CONFIG_FILE = "config.json"
-VERSION = "0.0.20"
+VERSION = "0.0.21"
 
 @dataclass
 class Config:
