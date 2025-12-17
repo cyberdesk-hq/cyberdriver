dd48750c3685adcec821d2c51af67914c69875b3
Alan Duong <alanduong07@gmail.com>
2025-12-16 16:03:25 -0800

    chore: Update version to '0.0.35' for release

--- cyberdriver.py
+++ cyberdriver.py
@@ -753,7 +753,7 @@ async def connect_with_headers(uri, headers_dict):
 
 CONFIG_DIR = ".cyberdriver"
 CONFIG_FILE = "config.json"
-VERSION = "0.0.34"
+VERSION = "0.0.35"
 
 @dataclass
 class Config:
